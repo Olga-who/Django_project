dd7bc1316481e4b3ff7f20a207b1e4f6f2f7f983
Olya Naboychenko <olyanaboychenko@gmail.com>
2025-03-25 14:34:23 +0300

    Login form and controller are ready

--- app/settings.py
+++ app/settings.py
@@ -136,14 +136,14 @@ MEDIA_URL = 'media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
 INTERNAL_IPS = [
     # ...
     "127.0.0.1",
     # ...
 ]
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'users.User'
