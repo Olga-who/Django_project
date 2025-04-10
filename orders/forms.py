140ef12e8da6b208ee8641323d947224bb3eafe9
Olya Naboychenko <olyanaboychenko@gmail.com>
2025-04-10 19:11:16 +0300

    Create order is readyy

--- orders/forms.py
+++ orders/forms.py
@@ -1,4 +1,4 @@
-from django import forms 
+from django import forms
 
 
 class CreateOrderForm(forms.Form):
@@ -6,8 +6,16 @@ class CreateOrderForm(forms.Form):
     first_name = forms.CharField()
     last_name = forms.CharField()
     phone_number = forms.CharField()
-    requires_delivery = forms.ChoiceField()
+    requires_delivery = forms.ChoiceField(
+        choices=[
+            ("0", False), 
+            ("1", True)
+            ]
+        )
     delivery_address = forms.CharField(required=False)
-    payment_on_get = forms.ChoiceField()
-
-
+    payment_on_get = forms.ChoiceField(
+        choices=[
+            ("0", "False"), 
+            ("1", "True")
+            ]
+        )
