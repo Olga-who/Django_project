ee66524d9cab53f64379757bb9f890441fa7cb1e
Olya Naboychenko <olyanaboychenko@gmail.com>
2025-04-10 14:32:23 +0300

    Admin panel setup

--- goods/models.py
+++ goods/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Categories(models.Model):
-    name = models.CharField(max_length=150, unique=True)
+    name = models.CharField(max_length=150, unique=True, verbose_name="Название")
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True)
 
     class Meta:
@@ -27,7 +28,7 @@ class Products(models.Model):
         default=0.00, max_digits=7, decimal_places=2, verbose_name="Цена"
     )
     discount = models.DecimalField(
-        default=0.00, max_digits=4, decimal_places=2, verbose_name="Скидка в процентах"
+        default=0.00, max_digits=4, decimal_places=2, verbose_name="Скидка в %"
     )
     quantity = models.PositiveBigIntegerField(default=0, verbose_name="Количество")
     category = models.ForeignKey(
@@ -42,6 +43,10 @@ class Products(models.Model):
 
     def __str__(self):
         return f"{self.name} Количество - {self.quantity}"
+    
+    def get_absolute_url(self):
+        return reverse("catalog:product", kwargs={"product_slug": self.slug})
+    
 
     def display_id(self):
         return f"{self.id:05}"
