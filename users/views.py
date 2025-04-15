f752af3572049fd04b417e676d50200eec0316c9
Olya Naboychenko <olyanaboychenko@gmail.com>
2025-04-15 11:55:51 +0300

    Final edits

--- users/views.py
+++ users/views.py
@@ -3,9 +3,12 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.contrib import auth, messages
 from django.urls import reverse
+from django.db.models import Prefetch
 
 from carts.models import Cart
+from orders.models import Order, OrderItem
 from users.form import ProfileForm, UserLoginForm, UserRegistrationForm
+import logging 
 
 
 def login(request):
@@ -72,9 +75,23 @@ def profile(request):
                 return HttpResponseRedirect(reverse('user:profile'))               
     else:
         form = ProfileForm(instance=request.user)
+
+    orders = (
+         Order.objects.filter(user=request.user)
+            .prefetch_related(
+                 Prefetch(
+                     "orderitem_set",
+                     queryset=OrderItem.objects.select_related("product"),
+                 )
+             )
+             .order_by("-id")
+    )
+    # AttributeError("Manager isn't accessible via Order instances")
+
     context = {
         'title': 'Home - Кабинет',
-        'form': form
+        'form': form,
+        "orders": orders
     }
     return render(request, 'users/profile.html', context)
 
